01416b00da4dee6c593e4f19c7cfc40ee08ce8a5
Robert Yin <bobertoyin@gmail.com>
2024-01-24 22:05:14 -0600

    support inline container for IconText

--- src/components/IconText.tsx
+++ src/components/IconText.tsx
@@ -1,9 +1,18 @@
 import { Text } from "@radix-ui/themes";
 import { PropsWithChildren } from "react";
 
-export default function IconText(props: PropsWithChildren): JSX.Element {
+interface IconTextProps {
+	inline?: boolean;
+}
+
+export default function IconText(
+	props: PropsWithChildren<IconTextProps>,
+): JSX.Element {
+	const { inline } = props;
 	return (
-		<Text style={{ display: "flex", alignItems: "center" }}>
+		<Text
+			style={{ display: inline ? "inline-flex" : "flex", alignItems: "center" }}
+		>
 			{props.children}
 		</Text>
 	);
